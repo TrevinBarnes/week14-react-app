61284c1a49f371089411506584d2ebd7469370b4
TrevinBarnes <TrevinBarnes@users.noreply.github.com>
2024-03-01 15:19:13 -0600

    added movie image, synopsis, and begging of ratings

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="MovieList">
+      <div className="Movie">
+        <h1 id="Title"></h1>
+        <img src='https://academymuseumstore.org/cdn/shop/products/movieposterredo32.jpg?v=1666936667' id='MovieCover'/>
+        <p>Revenge of the Sith is the third of the prequal trilogy. Telling the story of how anakin skywalker battles his fears which eventually lead him to becoming Darth Vader. 
+        This movie contains a lot of action and causes a variety of emotions. </p>
+        <div className='Rating'>
+          <button className='Stars'>1star</button>
+          <button className='Stars'>2star</button>
+          <button className='Stars'>3star</button>
+          <button className='Stars'>4star</button>
+          <button className='Stars'>5star</button>
+          <div className='ReviewList'>
+            <p>leave your opinions here</p>
+            <div className='Review' >
+
+            </div>
+          </div>
+          
+          
+        </div>
+
+
+      </div>
     </div>
+
+
+
   );
 }
 
+
 export default App;
+
+
+/*
+Using what you've learned this week, create a page of an application that enables users to vote and leave reviews on movies.
+
+All data necessary for this assignment, including Movies and Reviews, can be pulled from an API or you can create the Movies and their Reviews yourself.
+
+  Your application must include the following components:
+
+  - MovieList: a container for all the Movie components and their data.  
+
+  - Movie: a component that represents movie data (i.e. image, synopsis, rating, etc…)
+
+  - Stars: a one to five-star rating component that allows users to rate something 
+    (movies in this case, but remember that components are reusable, so you could use it elsewhere!)
+
+  - ReviewList: a container inside of a Movie that houses Review components.
+
+  - Review: A text review a user can leave on a movie.
+
+  - ReviewForm: a form at the bottom of a Movie component that allows users to leave reviews. 
+
+When submitted, the review should be added to the movie. All this data can be stored in an array, no networking or database needed for this assignment.
+*/
